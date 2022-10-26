6d63742797ce717203ea98af2dfb3671714f1ea3
Leonardo7Silva <leo7kisam@gmail.com>
2022-10-26 10:28:56 -0300

    merged 26/10 10h

--- src/controllers/hasgtagController.js
+++ src/controllers/hasgtagController.js
@@ -86,55 +86,6 @@ async function getHashtagPosts(req, res) {
     }
 }
 
-async function getHashtagPosts(req, res) {
-  const { authorization } = req.headers;
-  const token = authorization?.replace(`Bearer `, ``);
-  const { hashtag } = req.params;
 
-  try {
-    const query = await hashtagPosts(hashtag);
-    if (!token) {
-      return res.send(query.rows);
-    }
-    const user = await findUser(token);
-    const userLikeList = await findUserLikes(user);
-    const list = [];
-    for (let index = 0; index < query.rows.length; index++) {
-      const element = query.rows[index];
-      if (userLikeList[element.id] !== 1) {
-        list.push({
-          ...element,
-          isLiked: false,
-        });
-      } else
-        list.push({
-          ...element,
-          isLiked: true,
-        });
-    }
-    const totalLikesList = await totalLikes();
-    for (let index = 0; index < list.length; index++) {
-      const element = list[index];
-      if (!totalLikesList[element.id]) {
-        list[index] = {
-          ...element,
-          totalLikes: 0,
-        };
-        continue;
-      } else
-        list[index] = {
-          ...element,
-          totalLikes: totalLikesList[element.id],
-        };
-    }
-    res.status(200).send(list);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send({
-      message:
-        'An error occured while trying to fetch the posts, please refresh the page',
-    });
-  }
-}
 
 export { getHashtag, getHashtagPosts };
