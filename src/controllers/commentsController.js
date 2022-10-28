b045f46f24d746e45898f6cbb95dabab3afe3ce3
Daivison Morais <daivison13pinheiro@gmail.com>
2022-10-28 00:05:02 -0300

    rotas get e post comments plenamente em uso

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -3,27 +3,23 @@ import {
   getComments,
 } from "../repositories/commentsRpositories.js";
 import { STATUS_CODE } from "../enums/statusCodes.js";
-import { connection } from "../database/db.js";
 
 async function insertComments(req, res) {
   const { comment } = req.body;
   const { postId } = req.params; //id do post que foi comentado
 
-  const idPost = await connection.query(
-    `SELECT id FROM posts WHERE $1 = posts.id;`,
-    [postId]
-  );
-
-  if (!comment || idPost.length > 0) {
-    return res.sendStatus(STATUS_CODE.BAD_REQUEST);
+  if (!comment) {
+    return res
+      .status(STATUS_CODE.BAD_REQUEST)
+      .send({ error: "message invalid" });
   }
 
   try {
     const user = res.locals.user;
     await createCommentsRepositories({
       comment: comment,
-      userId: user.id, //userId é de quem ta comentando.
-      postId: idPost,
+      userId: user.id,
+      postId: postId,
     });
 
     res.sendStatus(STATUS_CODE.CREATED);
@@ -37,8 +33,9 @@ async function listComments(req, res) {
   const { postId } = req.params;
 
   try {
-    await getComments({ postId });
-    res.sendStatus(STATUS_CODE.OK);
+    const listComments = await getComments({ postId });
+
+    res.send(listComments);
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
--- src/repositories/commentsRpositories.js
+++ src/repositories/commentsRpositories.js
@@ -3,22 +3,23 @@ import { connection } from "../database/db.js";
 async function createCommentsRepositories({ userId, comment, postId }) {
   await connection.query(
     `INSERT INTO comments ("userId", "message", "postId") VALUES ($1, $2, $3);`,
-    [userId, comment, postId.rows[0].id]
+    [userId, comment, postId]
   );
 }
 
 async function getComments({ postId }) {
-  await connection.query(
+  const lstComments = await connection.query(
     `SELECT 
             comments.message, 
             users.picture, 
-            users.id, 
+            users.id AS "idUserComment", 
             users.name 
         FROM comments 
         JOIN users ON comments."userId" = users.id 
         WHERE comments."postId" = $1;`,
     [postId]
   );
+  return lstComments.rows;
 }
 
 export { createCommentsRepositories, getComments };
